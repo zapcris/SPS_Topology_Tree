7f20a3e0978a89390e5828f21ac8d21f127618b3
zapcris <akshayavhad89@gmail.com>
2022-03-22 18:52:30 +0100

    function for creating spanning tree

--- Fitness_Function.py
+++ Fitness_Function.py
@@ -1,16 +1,24 @@
 import networkx as nx
 
+from Func_weighted_spanning_tree import euclidean_dist
 
-def fitness_function(T, batch_list, PI_weight):
+
+def fitness_function(T, batch_list, PI_weight,pos):
     PI_cost = []
+    EU_cost = []
 
     for i in range(len(batch_list)):
         cost = 0.0
+        cost2 = 0.0
         for j in range(len(batch_list[i]) - 1):
             #print(f"source{batch_list[i][j]}and target {batch_list[i][j + 1]}")
             cost += nx.dijkstra_path_length(T, batch_list[i][j], batch_list[i][j + 1])
+            cost2 += euclidean_dist(pos[batch_list[i][j]][0],pos[batch_list[i][j]][1],pos[batch_list[i][j+1]][0], pos[batch_list[i][j+1]][1])
+
 
         PI_cost.append(round(cost * (PI_weight[i] / 100)))
+        EU_cost.append(round(cost2 * (PI_weight[i] / 100)))
 
-    batch_cost = sum(PI_cost)
-    return batch_cost  # ,PI_cost
+    batch_tree_cost = sum(PI_cost)
+    batch_eu_cost = sum(EU_cost)
+    return batch_tree_cost, batch_eu_cost   # ,PI_cost
--- main.py
+++ main.py
@@ -40,8 +40,10 @@ def rand_index(gen):
 #          11: (28, 7), 12: (33, 12), 13: (24, 13), 14: (0, 3), 15: (25, 22),
 #          16: (12, 14), 17: (27, 25), 18: (21, 22), 19: (32, 31), 20: (26, 33)}
 
-
 G_pos = {1: (9, 4), 2: (18, 13), 3: (22, 8), 4: (18, 10), 5: (13, 0), 6: (15, 4), 7: (24, 22), 8: (12, 6), 9: (10, 11), 10: (14, 16), 11: (26, 15), 12: (30, 4), 13: (17, 17), 14: (0, 2), 15: (19, 24), 16: (22, 12), 17: (26, 20), 19: (23, 33), 20: (27, 28)}
+
+
+
 
 Batch_sequence = [[1, 5, 9, 10, 2, 11, 13, 15, 7, 20],
                   [1, 2, 7, 3, 5, 6, 8, 9, 13, 15, 19, 20],
@@ -148,15 +150,18 @@ time.sleep(1)  # Pause 1 seconds
 print("pause ended")
 
 for i, (chr_Tree, pos) in enumerate(zip(random_pop, tree_pos)):
-    random_fitness.append(fitness_function(chr_Tree, Batch_sequence, PI_weight))
+    random_fitness.append(fitness_function(chr_Tree, Batch_sequence, PI_weight,pos))
     time.sleep(0.05)
     # print(i, random_fitness[i])
 
 cross_gen_fitness.append(random_fitness)
+
 # print(random_pop[20].edges())
 ## Choose the fittest spanning tree####
 
-print("Fitness values for population", random_fitness)
+print("TREE Fitness values for population", random_fitness)
+print("Euclidean Fitness values for population", random_fitness)
+sys.exit()
 sorted_fitness = sorted(random_fitness)
 pIndex_1 = random_fitness.index(sorted_fitness[0])
 pIndex_2 = random_fitness.index(sorted_fitness[1])
